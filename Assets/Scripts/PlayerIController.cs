8345839da7b552b50574f67c9794106bc964762e
Artem <artem.my3.pull@gmail.com>
2025-08-06 02:27:09 +0300

    Hit

--- Assets/Scripts/PlayerIController.cs
+++ Assets/Scripts/PlayerIController.cs
@@ -74,18 +74,13 @@ public class PlayerController : MonoBehaviour {
         enabled = true;
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Obstacle")) {
-            Hit();
-        }
-    }
-
     private void OnDestroy() {
         inputController.MovementReceived -= OnMove;
         inputController.JumpPressed -= OnJump;
         inputController.Dispose();
     }
 }
+
 
 
 
